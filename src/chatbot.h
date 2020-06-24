947c50278b087489bcb0d38a07a1594996adb9be
Ratul Saha <ratul.saha@mavenir.com>
2020-06-23 21:46:27 -0500

    Fixing bugs in ChatBot

---
+++ src/chatbot.h
@@ -0,0 +1,53 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+  
+    // rule of five
+    // Copy Constructor (Implementing Deep Copying Policy)
+    ChatBot(const ChatBot &source);
+    // Copy Assignment Operator
+    ChatBot &operator=(const ChatBot &source);
+    // Destructor (Already there - Check if need any modification)
+    // Move Constructor
+    ChatBot(ChatBot &&source);
+    // Move Assignment Operator.
+    ChatBot &operator=(ChatBot &&source);
+
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
---
+++ src/graphnode.cpp
@@ -0,0 +1,56 @@
+
+  
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+
+    // Remove Initial Seg Fault by commenting next line
+    // delete _chatBot;
+
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+// Change MoveChatBotHere argument type
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = chatbot;
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    // not needed
+    //_chatBot = nullptr; // invalidate pointer at source
+}
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+
+
+    // Get the edge
+    return _childEdges[index].get();
+
+}
+
